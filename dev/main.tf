351c782c9c869423f2a3b7d44918d1a0f0599ab7
Allan Tenison <atenison@bokf.com>
2021-11-17 13:55:01 -0600

    added argocd module

--- dev/main.tf
+++ dev/main.tf
@@ -139,3 +139,14 @@ module "eks" {
     GithubOrg  = "terraform-aws-modules"
   }
 }
+
+################################################################################
+# Argo CD
+################################################################################
+
+module "argo_cd" {
+  source = "runoncloud/argocd/kubernetes"
+
+  namespace       = "argocd"
+  argo_cd_version = "1.8.7"
+}
